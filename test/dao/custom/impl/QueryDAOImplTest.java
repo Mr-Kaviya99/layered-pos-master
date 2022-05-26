ce3635888b2cee8c9332311a9ccc4eed831a6682
Mr-Kaviya99 <kavinduhasintha9@gmail.com>
2022-05-26 22:13:47 +0530

    updated

---
+++ test/dao/custom/impl/QueryDAOImplTest.java
@@ -0,0 +1,17 @@
+package dao.custom.impl;
+
+import dao.DAOFactory;
+import dao.DAOType;
+import dao.SuperDAO;
+import dao.custom.QueryDAO;
+import entity.CustomEntity;
+
+import javax.management.Query;
+
+class QueryDAOImplTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
